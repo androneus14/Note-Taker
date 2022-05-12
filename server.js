fcc172286cfd4d4a0c42b4679b0882e90f17a6ef
Andy Nguyen <andynguyen_3@hotmail.com>
2022-05-12 19:10:29 +1000

    app not working. removing uuid

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
-const uuid = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 4091;
@@ -34,7 +33,6 @@ app.get("/api/notes", (req, res) => {
 // posts new notes and joins it in db.json
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
-    newNote.id = uuid();
     notes.push(newNote);
     fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes));
     res.json(newNote);
