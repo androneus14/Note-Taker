d2c0716fc38fe760a1d22db13cf0123625786b70
Andy Nguyen <andynguyen_3@hotmail.com>
2022-05-12 19:43:32 +1000

    finished working code

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-// posts new notes and joins it in db.json
+// post new notes and joins it in db.json
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     notes.push(newNote);
