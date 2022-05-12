d7efd02da6a811e8807812900a637c53f0d94893
Andy Nguyen <andynguyen_3@hotmail.com>
2022-05-12 19:08:59 +1000

    trying to get UUID to work

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const notes = require("./db/db.json");
 const uuid = require("uuid");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4091;
 
 // Middleware
 app.use(express.json());
