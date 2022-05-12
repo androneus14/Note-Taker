26b92817f915c186f60a1c4542494079d6b5990d
Andy Nguyen <andynguyen_3@hotmail.com>
2022-05-12 15:30:44 +1000

    application not working. trying updated code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
